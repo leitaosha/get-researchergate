32e0d85337983870d3935fa44aca147e193bb18c
ForSmile0 <473153250@qq.com>
2023-10-31 22:43:58 +0800

    optimize code

---
+++ crawlerutils/DataClean.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@Author：ForSmile
+@Time：2023/10/31  22:29
+@Contact：473153250@qq.com
+"""
+import pandas as pd
+
+
+def pdFilter(data: pd.DataFrame):
+    pass
--- crawlerutils/ExportFormat.py
+++ crawlerutils/ExportFormat.py
@@ -9,7 +9,7 @@ from datetime import datetime
 import pandas as pd
 import config
 
-FileName = f"{config.ExportPath}{config.Url.split('/')[-1]} {datetime.now().strftime('%Y-%m-%d %H-%M-%S')}"
+FileName = f"{config.ExportPath}{config.Url.split('/')[-1]} {datetime.now().strftime('%Y%m%d %H%M')}"
 
 
 # CSV
